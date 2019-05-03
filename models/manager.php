b413dc6afbe4dd97145998536c2e2718460f2875
GrimGraysamas <ggraysamas@gmail.com>
2019-05-03 15:40:56 +0200

    Update du model manager
    Ajout d'une fonction pour extraire un seul champ d'une table

--- models/manager.php
+++ models/manager.php
@@ -6,4 +6,13 @@ class Manager
         $db = new PDO('mysql:host=bd5000024306.hosting-data.io;dbname=dbs19544;charset=utf8', 'dbu29033', 'JSBpjbscrmpbtd159!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return $db;
     }
+
+    public function getElement($tablename, $name) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT ? FROM ? ORDER BY id DESC');
+        $req->execute(array($name, $tablename));
+        $data = $req->fetch();
+
+        return $data;
+    }
 }
