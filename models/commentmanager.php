1297bcbc6f40f9e26c8734d2e2a5ccd8b75a7811
GrimGraysamas <ggraysamas@gmail.com>
2019-05-03 15:35:01 +0200

    Update du comment manager
    Ajout du CRUD

---
+++ models/commentmanager.php
@@ -0,0 +1,54 @@
+<?php
+require_once('model/manager.php');
+
+class CommentManager extends Manager
+{
+    public function createComment($postId, $author, $content) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('INSERT INTO comments(author, content, postid, postingtime) VALUES(?,?,?, NOW())');
+        $output = $req->execute(array($author, $content, $postId));
+
+        return $output;
+    }
+
+    public function readComment($postId, $commentId) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT * FROM comments WHERE postid = ? AND id = ?');
+        $req->execute(array($postId, $commentId));
+        $data = $req->fetch();
+
+        return $data;
+    }
+
+    public function updateComment($commentId, $name, $value) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE comments SET ? = ? WHERE id = ?');
+        $output = $req->execute(array($name, $value, $commentId));
+
+        return $output;
+    }
+
+    public function deleteComment($postId, $commentId) {
+        $db=$this->dbConnect();
+        $req = $db->prepare('DELETE FROM comments WHERE postid = ? AND id = ?');
+        $output = $req->execute(array($postId, $commentId));
+
+        return $output;
+    }
+
+    public function reportComment($commentId) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE comments SET reports = reports + 1 WHERE id = ?');
+        $output = $req->execute(array( $commentId));
+  
+        return $output;
+    }
+
+    public function  readFirstComments() {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT * FROM comments ORDER BY postingtime DESC LIMIT 5');
+
+        return $req;
+    }
+    
+}
