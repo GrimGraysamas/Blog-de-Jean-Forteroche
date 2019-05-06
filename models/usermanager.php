dd3b56c150619647ab14a7634dd3d4c5b52b78f3
GrimGraysamas <ggraysamas@gmail.com>
2019-05-06 14:31:39 +0200

    Update du user manager
    Creéation des fonctions CRUD.

---
+++ models/usermanager.php
@@ -0,0 +1,39 @@
+<?php 
+
+require_once('model/manager.php');
+
+class UserManager extends manager
+{
+    public function createAccount($username, $password) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('INSERT INTO accounts(username, password) VALUES(?, ?)');
+        $output = $req->execute(array($username, hash('sha512', $password)));
+
+        return $output;
+    }
+    
+    public function readAccount($username) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT * FROM accounts WHERE username = ?');
+        $req->execute(array($username));
+        $data = $req->fetch();
+
+        return $data;
+    }
+
+    public function updateAccount($username, $newpwd) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE accounts SET  password = ? WHERE username = ?');
+        $output = $req->execute(array($newpwd, $username));
+
+        return $output;
+    }
+
+    public function deleteAccount($username) {
+        $db = $this->dbConnect();
+        $req = $db->prepare('DELETE FROM accounts WHERE username = ?');
+        $output = $req->execute(array($username));
+
+        return $output;
+    }
+}
