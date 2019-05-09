2273d8168f3c64dc7c1d55f158e6b2f0f96cbc14
GrimGraysamas <ggraysamas@gmail.com>
2019-05-09 15:43:57 +0200

    Création du header

---
+++ views/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav class="navbar bgprimary contentsecondary">
+        <div class="nav-section-1">
+            <h4 class="subtitle nav-item contentadd1">Blog de Jean Forteroche</h4>
+        </div>
+        <div class="nav-section-2 nav-wrapper">
+            <div class="nav-item"><a href="index.php">Accueil</a></div>
+            <div class="nav-item"><a href="index.php?action=listallposts">Liste des billets</a></div>
+            <div class="nav-item"><a href="index.php?action=admin">Administration</a></div>
+        </div>
+    </nav>
+</header> 
