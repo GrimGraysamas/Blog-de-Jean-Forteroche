ef85730f5cfe467e804cf7876049acdb86be4fcd
GrimGraysamas <ggraysamas@gmail.com>
2019-05-09 15:47:30 +0200

    Mise à jour de l'index
    Ajout des actions readPost et listPosts

--- index.php
+++ index.php
@@ -5,8 +5,23 @@ session_start();
 require('controllers/backend.php');
 require('controllers/frontend.php');
 
-try {
-    listPosts();
+try { 
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'readpost') {
+            if (isset($_GET['postid']) && $_GET['postid'] > 0) {
+                readPost();
+            }
+            else {
+                throw new Exception('Aucun billet sélectionné');
+            }
+        }
+        elseif ($_GET['action'] == 'listallposts') {
+            listAllPosts();
+        }
+    }
+    else {
+        listLatestPosts();
+    }
 }
 catch (Exception $e) {
     echo 'Erreur : ' . $e->getMessage();
