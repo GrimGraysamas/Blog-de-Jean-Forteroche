5a163c3d7857ac3cfd653c1e433f5aea3d90db3a
GrimGraysamas <ggraysamas@gmail.com>
2019-05-13 13:30:49 +0200

    Correction de design

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,5 @@
-<footer class="fullCentered borderT">
+<footer class="fullCentered flexColumn">
+    <div class="divider bgdark"></div>
     <div class="container is-vertical">
         <p class="is-3 text-centered">Le blog de Jean Fortercohe est lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Reprehenderit, ab ratione unde mollitia ea sunt labore repellat aperiam commodi excepturi, repudiandae similique ipsa facilis voluptates! <br>Molestias ut iusto odit porro. Recusandae, earum atque rem numquam consequatur <br> nulla perferendis maxime quae et accusantium inventore labore fugiat debitis illo necessitatibus aut a unde pariatur quibusdam. <br>Ea corrupti asperiores at totam, suscipit excepturi.</p>
         <nav class="navbar fullCentered">
--- views/postView.php
+++ views/postView.php
@@ -2,7 +2,7 @@
 
 <?php ob_start();?>
 
-<section class="hero has-navbar">
+<section>
     <div class="container is-vertical is-large">
         <h2 class="title M50top M50bottom noMleft"><?= htmlspecialchars($post['title']); ?></h2>
         <h6 class="subtitle M25bottom M25top noMleft">Ecrit par <strong><?= htmlspecialchars($post['author']); ?></strong> le <?= htmlspecialchars($post['postingtime']); ?></h6>
@@ -15,13 +15,15 @@
     <form class="container is-vertical is-medium" action="index.php?action=addcomment&amp;postid=<?= $post['id'] ?>" method="post">
         <h2 class="subtitle">Ajouter un commentaire :</h2>
         <div class="container is-horizontal marginless alignEnd P25">
-            <div class="container alignCenter is-vertical marginless P25">
+            <div class="container is-vertical marginless P25">
                 <label for="author">Auteur :</label>
-                <input class="bordernone borderB contentadd1" type="text" id="author" name="author">
+                <input class="bordernone" type="text" id="author" name="author">
+                <div class="divider bgadd1 noMleft noMtop"></div>
             </div>
             <div class="container is-vertical marginless P25">
                 <label for="comment">Commentaire :</label>
-                <textarea class="bordernone borderB contentadd1" name="comment" id="comment" rows="1" cols="175"></textarea>
+                <textarea class="bordernone" name="comment" id="comment" rows="1" cols="175"></textarea>
+                <div class="divider bgadd1 noMleft noMtop"></div>
             </div>
             <div>
                 <input class="button is-rounded is-outlined primary-add1" type="submit" value="Soumettre">
@@ -29,7 +31,7 @@
         </div>
     </form>
 </section>
-<section class="section P50top P50bottom">
+<section class="P50top P50bottom">
     <h1 class="subtitle P50bottom M25left">Commentaires :</h1>
     <?php
     while ( $data = $comments->fetch())
