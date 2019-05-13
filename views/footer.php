f4e58f82ecd0b8f5a343bd7a7581a70e85e26e9f
GrimGraysamas <ggraysamas@gmail.com>
2019-05-13 12:15:49 +0200

    Ajout du footer

---
+++ views/footer.php
@@ -0,0 +1,12 @@
+<footer class="fullCentered borderT">
+    <div class="container is-vertical">
+        <p class="is-3 text-centered">Le blog de Jean Fortercohe est lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Reprehenderit, ab ratione unde mollitia ea sunt labore repellat aperiam commodi excepturi, repudiandae similique ipsa facilis voluptates! <br>Molestias ut iusto odit porro. Recusandae, earum atque rem numquam consequatur <br> nulla perferendis maxime quae et accusantium inventore labore fugiat debitis illo necessitatibus aut a unde pariatur quibusdam. <br>Ea corrupti asperiores at totam, suscipit excepturi.</p>
+        <nav class="navbar fullCentered">
+            <div class="nav-wrapper">
+                <div class="nav-item"><a class="textdecorationnone" href="index.php">Accueil</a></div>
+                <div class="nav-item"><a class="textdecorationnone" href="index.php?action=listallposts">Liste des billets</a></div>
+                <div class="nav-item"><a class="textdecorationnone" href="index.php?action=admin">Administration</a></div>
+            </div>
+        </nav>
+    </div>
+</footer>
